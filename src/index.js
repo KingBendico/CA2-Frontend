598cc66eaced53b3144d443c055dbf21ef6dfd82
Frederik Riisager Johnsen <riisager@pop-os.localdomain>
2020-10-21 11:06:56 +0200

    added request/response formats to documentation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
 import 'bootstrap/dist/css/bootstrap.css'
-import jokes from "./jokes";
 
-const allJokes = jokes.getJokes().map(joke => "<li>"+joke+"</li>");
-document.getElementById("jokes").innerHTML = allJokes.join("");
 
