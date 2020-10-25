959c2fd44c3e4448a3a0ead461422dd8e7c9077e
KingBendico <cph-bc105@cphbusiness.dk>
2020-10-25 10:05:04 +0100

    last functions

--- src/index.js
+++ src/index.js
@@ -8,23 +8,55 @@ const checkError = function (res) {
     }
 }
 
-
-function filterByHobby(people, hobbyId) {
-    return people.filter(
-        person => person.hobbies.some(
-            hobby => hobby.id === hobbyId
-        )
-    );
-}
-
-
-const filterByHobby = function () {
-    const input = document.getElementById("hobby-button").value; //TODO, take input from HTML page
+const getPersonsByHobby = function () {
+    const input = document.getElementById("???").value; //TODO, take input from HTML page
     const result = fetch("https://benjamincholeva.dk/api/persons/hobby/" + input)
         .then(checkError)
         .then(data => {
-            let pHobby = {
-                method: "GET",
+            document.getElementById("???").innerHTML = data.map(SingleDataToTable);
+        })
+    
+}
+
+const getPersonsByZip = function () {
+    const input = document.getElementById("???").value;  //TODO, take input from HTML page
+    const result = fetch("https://benjamincholeva.dk/api/persons/zip/" + input)
+        .then(checkError)
+        .then(data => {
+            document.getElementById("???").innerHTML = data.map(SingleDataToTable);
+        })
+    
+}
+
+const getPersonByPhone = function(){
+    const input = null; //TODO, take input from HTML page
+    const result = fetch("https://benjamincholeva.dk/api/persons/person/" + input)
+        .then(checkError)
+        .then(data => {
+            document.getElementById("???").innerHTML = SingleDataToTable(data);
+        })
+}
+
+const SingleDataToTable = function(data){
+    return "<tr>"+
+            "<td>" + data.firstName + " " + data.lastName + "</td"+
+            "<td" + data.email + "</td>"+
+            "<td>" + data.phones.join(", ") + "</td>"+
+            "<td>" + data.address.street + ", " + data.address.info + "</td>"+
+            "<td>" + data.hobbies.map(hobbyMapper) + "</td>"+
+            "</tr>";
+}
+
+const hobbyMapper = function(hobby){
+    return "<a href=\"" + hobby.link + "\">" + hobby.name + "</a>";
+}
+
+
+
+
+
+
+/*          method: "GET",
                 headers: {
                     'Accept': 'application/json',
                     'Content-Type': 'application/json'
@@ -64,5 +96,5 @@ const filterByHobby = function () {
                 },
         )};
     });
-}
 
+    /*
